e0e5a025db5f0d6ba71c2f34b7f8f0459724d6dd
Kamil Polawski <kamil.polawski@protonmail.com>
2024-05-06 20:23:33 +0200

    Add missing bean name for mock audit aware

--- src/main/java/com/loanshark/accounts/audit/AuditAwareImpl.java
+++ src/main/java/com/loanshark/accounts/audit/AuditAwareImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
-@Component
+@Component("auditAwareImpl")
 public class AuditAwareImpl implements AuditorAware<String> {
     @Override
     public Optional<String> getCurrentAuditor() {
