2eedebe73bc9deb1ce34b2caa10c804b53648c85
Kamil Polawski <kamil.polawski@protonmail.com>
2024-05-20 16:42:55 +0200

    add circuit breaker resilience dependendency and config

--- src/main/java/com/loanshark/accounts/external/loan/LoanFeignClient.java
+++ src/main/java/com/loanshark/accounts/external/loan/LoanFeignClient.java
@@ -1,13 +1,16 @@
 package com.loanshark.accounts.external;
 
+import com.loanshark.accounts.external.dto.LoanDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient("loans")
+@FeignClient(name = "loans", fallback = Object.class)
 public interface LoanFeignClient {
     @GetMapping(value = "/api/loans", consumes = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<?> getLoanDetail(@RequestParam String mobileNumber);
+    ResponseEntity<LoanDto> getLoanDetail(@RequestParam String mobileNumber);
 }
+
+
